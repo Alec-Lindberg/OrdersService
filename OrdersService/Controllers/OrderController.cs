68a31e563af41742305e9d6febefdffbe36155cf
alindberg1 <alindberg1@LABSEC08-4LMJ>
2024-10-22 10:40:10 -0500

    Add project files.

---
+++ OrdersService/Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OrdersService.Models;
+using ProductService.Models;
+
+namespace OrdersService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private static List<Order> orders = new List<Order>
+    {
+        new Order { Id = 1, OrderDate = DateTime.Now, CustomerName = "John Doe", Products = new List<Product> { new Product { Id = 1, Name = "Product A", Price = 10.0m } } }
+    };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Order>> Get() => Ok(orders);
+    }
+}
---
+++ ProductService/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductService.Models;
+
+namespace ProductService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private static List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Product A", Price = 10.0m },
+            new Product { Id = 2, Name = "Product B", Price = 20.0m }
+        };
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get() => Ok(products);
+    }
+}
